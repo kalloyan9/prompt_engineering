0093ab104ce556a35a4ce87f70559d0d59f6c252
Kaloyan Mehandzhiyski <kalloyan9@gmail.com>
2025-05-13 10:05:07 +0300

    Update run_openain_grader.bat

--- ai_grader/run/run_openain_grader.bat
+++ ai_grader/run/run_openain_grader.bat
@@ -1,6 +1,6 @@
 @echo off
 cd ..
-echo Running OpenAI grader script...
+echo Running OpenAI grader script with chat gpt api...
 python src\grader_openai.py > output_openai.txt
 echo Output saved to output_openai.txt
 pause
